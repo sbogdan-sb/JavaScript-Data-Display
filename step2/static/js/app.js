e7cf2c945c775f213efbcce439edad5e4390d1bb
sbogdan60 <sbogdan.sb@gmail.com>
2019-04-28 15:54:29 -0700

    step 2 functioning

--- step2/static/js/app.js
+++ step2/static/js/app.js
@@ -1,11 +1,15 @@
 // from data.js
 var tableData = data;
+var filterSelection = 'datetime';
 
 // Register callback function for search button
 d3.select("#filter-btn").on("click", doButtonAction);
 
+d3.selectAll(".filterOption").on("click", doFilterAction);
+
 // Write each sighting given in the initial data object to table
 data.forEach(writeTable);
+
 
 // Writes a row into table from a ufo sighting object 
 function writeTable(sighting) {
@@ -22,19 +26,28 @@ function writeTable(sighting) {
 //matching sightings
 function doButtonAction () {
     var table = d3.select("#ufo-table");
-    var searchDate = d3.select("#datetime").property("value");
+    var searchValue = d3.select("#datetime").property("value");
 
     // Clear table
     table.selectAll('td').remove();      
     
     // Create new array of sightings that date matches user search date
-    var filtered = data.filter(sighting => { return sighting.datetime == searchDate});
-    
+    var filtered = data.filter(sighting => { return sighting[filterSelection] == searchValue});
+
     // Write filtered sightings to table
     filtered.forEach(writeTable);
 
     // Clear input textbox after each search
     document.getElementById('datetime').value = "";
+
+    return;
+}
+
+function doFilterAction () {
+    var menuLabel = d3.select("label")
+    filterSelection = d3.select(this).text();
+
+    menuLabel.text("Enter a " + filterSelection);
 
     return;
 }
